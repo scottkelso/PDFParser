177519dfea682671e06cf5f6e828c483f987b9d0
scottkelso <scottkelso07@hotmail.com>
2024-02-27 19:26:50 +1300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Docnet.Core;
+using Docnet.Core.Models;
+using Docnet.Core.Readers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Create a reader from the file bytes.
+        try
+        {
+            using (var docReader = DocLib.Instance.GetDocReader(@"..\..\..\test.pdf", new PageDimensions()))
+            {
+                ReadFile(docReader);
+            }
+        } catch(FileNotFoundException ex) 
+        {
+            Console.WriteLine("File not found!");
+            Console.WriteLine(ex);
+        }
+    }
+
+    private static void ReadFile(IDocReader docReader)
+    {
+        for (var i = 0; i < docReader.GetPageCount(); i++)
+        {
+            using (var pageReader = docReader.GetPageReader(i))
+            {
+                var text = pageReader.GetText();
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
+
